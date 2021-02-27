875b0d281a2562b94ed7cb352119526f49a1206b
Leonardo Perez <leoperez0782@hotmail.com>
2021-02-27 18:38:04 -0300

    agregado provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_scanner/src/pages/home_page.dart';
 import 'package:qr_scanner/src/pages/map_page.dart';
+import 'package:qr_scanner/src/providers/ui_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QR Scanner',
-      initialRoute: 'home',
-      routes: {'home': (_) => HomePage(), 'map': (_) => MapPage()},
-      theme: ThemeData(
-          primaryColor: Colors.deepPurple,
-          floatingActionButtonTheme: FloatingActionButtonThemeData(
-              backgroundColor: Colors.deepPurple)),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UiProvider(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR Scanner',
+        initialRoute: 'home',
+        routes: {'home': (_) => HomePage(), 'map': (_) => MapPage()},
+        theme: ThemeData(
+            primaryColor: Colors.deepPurple,
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+                backgroundColor: Colors.deepPurple)),
+      ),
     );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_scanner/src/pages/direcciones_page.dart';
+import 'package:qr_scanner/src/pages/mapas_page.dart';
+import 'package:qr_scanner/src/providers/ui_provider.dart';
 import 'package:qr_scanner/src/widgets/custom_navigatorbar.dart';
 import 'package:qr_scanner/src/widgets/scan_button.dart';
 
@@ -16,12 +20,31 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text('HomePage'),
-      ),
+      body: _HomePageBody(),
       bottomNavigationBar: CustomNavigationBar(),
       floatingActionButton: ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Obtener selectedMenuopt
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    //cambia para mostrar pagina respectiva
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    switch (currentIndex) {
+      case 0:
+        return MapasPage();
+
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
   }
 }
