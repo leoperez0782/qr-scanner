09eddc07437b07eb055878afbfead65fec92ed0e
Leonardo Perez <leoperez0782@hotmail.com>
2021-02-28 21:15:31 -0300

    funcionalidad del mapa agregada

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -1,19 +1,72 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'package:qr_scanner/src/models/scan_model.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({Key key}) : super(key: key);
 
   @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  MapType mapType = MapType.normal;
+  @override
   Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
+    final CameraPosition _puntoInicial = CameraPosition(
+      target: scan.getLatLng(),
+      zoom: 17.5,
+      tilt: 50.0,
+    );
+
+    //MArcadores
+    Set<Marker> markers = Set<Marker>();
+    markers.add(
+        Marker(markerId: MarkerId('geo-location'), position: scan.getLatLng()));
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.location_disabled),
+            onPressed: () async {
+              final GoogleMapController controller = await _controller.future;
+              controller.animateCamera(CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                      target: scan.getLatLng(), zoom: 17.5, tilt: 50)));
+            },
+          )
+        ],
       ),
-      body: Center(
-        child: Text(scan.valor),
+      body: GoogleMap(
+        mapType: mapType,
+        markers: markers,
+        initialCameraPosition: _puntoInicial,
+        onMapCreated: _onMapCreated,
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.layers),
+        onPressed: () {
+          if (this.mapType == MapType.normal) {
+            mapType = MapType.satellite;
+          } else {
+            mapType = MapType.normal;
+          }
+          setState(() {});
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    this._controller.complete(controller);
   }
 }
